fbb055f5f5d0d884d90a96849cb8ffcb9e474876
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-05-09 22:36:46 +0530

    :tada: sidebar update with dynamic button layout.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import { NavLinks } from "@/data/NavLinks";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Sidebar() {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
-    <nav className="min-h-screen w-16 md:w-80 p-2 border-r-2">
-      <ul className="grid gap-5 place-content-center">
-        <Image width={500} height={500} src="https://is.gd/3xnKDd" alt="twitter clone" className="w-12 h-12" />
+    <nav className="min-h-screen w-16 md:w-80 p-2 border-r-2 flex flex-col justify-between">
+      <ul className="grid gap-10 place-items-start">
+        <Link href="/" className="mb-10">
+          <Image
+            width={500}
+            height={500}
+            src="https://is.gd/3xnKDd"
+            alt="twitter clone"
+            className="w-12 h-12"
+          />
+        </Link>
         {NavLinks.map((item) => (
           <li key={item.id}>
             <Link
@@ -20,6 +32,17 @@ export default function Sidebar() {
           </li>
         ))}
       </ul>
+      <div>
+        {authenticated ? (
+          <button className="w-full bg-red-500 hover:bg-red-600 font-semibold px-4 py-2 rounded-md">
+            Logout
+          </button>
+        ) : (
+          <button className="w-full border-2 rounded-full font-semibold px-4 py-2">
+            Sign In
+          </button>
+        )}
+      </div>
     </nav>
   );
 }
